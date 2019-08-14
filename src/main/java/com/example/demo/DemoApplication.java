66697af33cc38da71f72a0e6cc31abaa34639fd9
Li Kai <1619285202@qq.com>
2019-08-14 20:15:19 +0800

    测试更新版本

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@GetMapping("/foo")
+	String foo() {
+		return "drone build ok";
+	}
+
 
 
 }
